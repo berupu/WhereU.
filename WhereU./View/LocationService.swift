4b6d026358005f52e2845ba64541a3a81f47a97e
Rupu <rupuislamashraful7@gmail.com>
2021-12-17 21:39:45 +0600

    LocationService .. On process

---
+++ WhereU./View/LocationService.swift
@@ -0,0 +1,25 @@
+//
+//  LocationService.swift
+//  WhereU.
+//
+//  Created by be RUPU on 17/12/21.
+//
+
+//import MapKit
+//import CoreLocation
+//
+//struct LocationService {
+//
+//    static let shared = LocationService()
+//    let mapView = MKMapView()
+//
+//     func addAnnotation(for user: String, with coordinate: CLLocation){
+//        let annotation = MKPointAnnotation()
+//        annotation.coordinate = CLLocationCoordinate2D(latitude: coordinate.coordinate.latitude, longitude: coordinate.coordinate.longitude)
+//        annotation.title = user
+//        mapView.addAnnotation(annotation)
+//        mapView.selectAnnotation(annotation, animated: true) //annotation animation
+//    }
+//
+//
+//}
